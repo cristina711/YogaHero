57fc798f8df0c12bb587dfeb0e1f426ed05d91c7
qian(cristina)yang_lin <cristina711@outlook.com>
2018-01-13 17:54:24 -0800

    Initial Commit

---
+++ YogaHero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YogaHero
+//
+//  Created by Qian Yang on 1/13/18.
+//  Copyright © 2018 Qian Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
