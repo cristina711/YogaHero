3f04d560a7e148b36b84ef22143a477b4e3072f5
Jihun Kang <j_kang9@u.pacific.edu>
2018-01-15 13:42:04 -0800

    Finished with UI

--- YogaHero/ViewController.swift
+++ YogaHero/ViewController.swift
@@ -1,22 +1,47 @@
 //
 //  ViewController.swift
-//  PoseGame
+//  YogaHero
 //
-//  Created by Jihun Kang on 1/12/18.
-//  Copyright © 2018 Jihun Kang. All rights reserved.
+//  Created by Qian Yang on 1/13/18.
+//  Copyright © 2018 Qian Yang. All rights reserved.
 //
 
 import UIKit
 import CoreMotion
 
+
+
+var motionManager = CMMotionManager()
+let opQueue = OperationQueue()
+
+// variables that need in all the views  have to be here for global use 
+
 class ViewController: UIViewController {
     
     var motionManager = CMMotionManager()
     let opQueue = OperationQueue()
+
+    
+    var poseimages = [""]
+
+    @IBOutlet weak var gameNameLabel: UILabel!
+    
+    @IBOutlet weak var images: UIImageView!
+    
+  
+    
+    @IBOutlet weak var gameStateLabel: UILabel!
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton){
+        
+       performSegue(withIdentifier: "segue", sender: self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view, typically from a nib.
         if motionManager.isDeviceMotionAvailable {
             print("We can detect device motion")
             startReadingMotionData()
@@ -25,6 +50,7 @@ class ViewController: UIViewController {
             print("We cannot detect device motion")
         }
     }
+
     
     func startReadingMotionData() {
         // set read speed
@@ -74,4 +100,14 @@ class ViewController: UIViewController {
     }
     
 }
+       
+
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+
 
