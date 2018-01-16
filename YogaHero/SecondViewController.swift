2325701a0c1eca0150db2e78b4ba0969ed290e73
dantails <jesus.urias90@gmail.com>
2018-01-15 18:15:18 -0800

    Added functionality to vertical posses

--- YogaHero/SecondViewController.swift
+++ YogaHero/SecondViewController.swift
@@ -90,27 +90,41 @@ class SecondViewController: UIViewController {
                     print ("Roll: ", self.degrees(mydata.attitude.roll))
                     print ("Yaw: ", self.degrees(mydata.attitude.yaw))
                 } else {
-//                    if abs(myPitch! - self.degrees(mydata.attitude.pitch)) > 4 {
-//                        print ("Pitch", self.degrees(mydata.attitude.pitch))
-//                    }
-                    if abs(myRoll! - self.degrees(mydata.attitude.roll)) < 4 {
-                        print ("Roll", self.degrees(mydata.attitude.roll))
-                        keepSteady += 1
-                        DispatchQueue.main.async {
-                            self.gameStateLabel.text = "You have held it for \(keepSteady) seconds."
+                    if self.poses[self.poseIndex].2 == "flat" {
+                        if abs(myRoll! - self.degrees(mydata.attitude.roll)) < 4 {
+                            print ("Roll", self.degrees(mydata.attitude.roll))
+                            keepSteady += 1
+                            DispatchQueue.main.async {
+                                self.gameStateLabel.text = "You have held it for \(keepSteady) seconds."
+                            }
+                        }
+                        else {
+                            print ("No change detected")
+                            keepSteady = 0
+                            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                            DispatchQueue.main.async {
+                                self.gameStateLabel.text = "Keep it straight"
+                            }
                         }
                     }
-//                    if abs(myYaw! - self.degrees(mydata.attitude.yaw)) > 4 {
-//                        print ("Yaw", self.degrees(mydata.attitude.yaw))
-//                    }
-                    else {
-                        print ("No change detected")
-                        keepSteady = 0
-                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-                        DispatchQueue.main.async {
-                            self.gameStateLabel.text = "Keep it straight"
+                    if self.poses[self.poseIndex].2 == "vertical" {
+                        if abs(myPitch! - self.degrees(mydata.attitude.pitch)) < 4 && abs(myYaw! - self.degrees(mydata.attitude.yaw)) < 4 {
+                            print ("Roll", self.degrees(mydata.attitude.roll))
+                            keepSteady += 1
+                            DispatchQueue.main.async {
+                                self.gameStateLabel.text = "You have held it for \(keepSteady) seconds."
+                            }
+                        }
+                        else {
+                            print ("No change detected")
+                            keepSteady = 0
+                            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                            DispatchQueue.main.async {
+                                self.gameStateLabel.text = "Keep it straight"
+                            }
                         }
                     }
+                    
                 }
                 if keepSteady >= 10{
                     DispatchQueue.main.async {
